8b80c310ce807a0076083c0a51018cc043e3fc46
stever40 <stever40@yahoo.com>
2020-03-26 20:13:08 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -83,59 +83,21 @@ class ServiceNowAdapter extends EventEmitter {
     this.healthcheck();
   }
 
-/**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-        this.emitOffline();
-        log.error(`ServiceNow: Instance is unavailable.  ID: stever ${JSON.stringify(error)}`); // for debugging
-        // log.error('ServiceNow: Instance is unavailable.  ID:') //+ this.id);
-        return error;
-
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-        this.emitOnline()
-        // log.info(`ServiceNow: Instance is available.  ID: stever ${JSON.stringify(result)}`); // for debugging
-        return result;
-   }
- });
-}
+  /**
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -146,8 +108,7 @@ healthcheck(callback) {
    */
   emitOffline() {
     this.emitStatus('OFFLINE');
-    // log.warn('ServiceNow: Instance is unavailable.');
-    log.error(`ServiceNow: Instance is unavailable.  ID: stever ${this.id}`); // for debugging
+    log.warn('ServiceNow: Instance is unavailable.');
   }
 
   /**
@@ -159,8 +120,7 @@ healthcheck(callback) {
    */
   emitOnline() {
     this.emitStatus('ONLINE');
-    // log.info('ServiceNow: Instance is available.');
-    log.info(`ServiceNow: Instance is available.  ID: stever ${this.id}`) // + String(this.id)); //+ this.id);
+    log.info('ServiceNow: Instance is available.');
   }
 
   /**
@@ -192,14 +152,11 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-      this.connector.get((data, error) => {
+      this.get((data, error) => {
         if (error) {
-          //console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-          return callback(error);
+          console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
         }
-        //console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`);
-        
-        return callback(data);
+        console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`);
       });
     }    
     
@@ -222,11 +179,9 @@ healthcheck(callback) {
      */
       this.post((data, error) => {
           if (error) {
-            // console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
-            return callback(error);
+            console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
           }
-          // console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`);
-          return callback(data);
+          console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`);
         });    
     }
 

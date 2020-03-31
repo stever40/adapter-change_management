2e5ac2a2b98ae837efe0f971dd8f69612bec30aa
stever40 <stever40@yahoo.com>
2020-03-31 01:37:36 +0000

    Final Project

--- connector.js
+++ connector.js
@@ -58,7 +58,7 @@ class ServiceNowConnector {
    get(callback) {
     let getCallOptions = this.options;
     getCallOptions.method = 'GET';
-    getCallOptions.query = 'sysparm_limit=1';
+    getCallOptions.query = 'sysparm_limit=2';
     this.sendRequest(getCallOptions, (results, error) => callback(results, error));
   }
 
